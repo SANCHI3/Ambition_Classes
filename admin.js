835d81bffb77404787a010cf4d9c177af3e91cea
SANCHI3 <167639420+SANCHI3@users.noreply.github.com>
2026-04-28 09:43:07 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -179,9 +179,19 @@ function resetFilters(){
 
 function editStudent(id){
 
+    console.log("Editing ID:", id);
+
     fetch(`${BASE_URL}/api/student/${id}`)
-    .then(res => res.json())
+    .then(res => {
+        console.log("Status:", res.status);
+
+        if(!res.ok){
+            throw new Error("API failed");
+        }
+        return res.json();
+    })
     .then(s => {
+        console.log("Data:", s);
 
         document.getElementById("studentName").value = s.name;
         document.getElementById("studentMobile").value = s.studentMobile;
@@ -191,8 +201,11 @@ function editStudent(id){
         document.getElementById("paidAmount").value = s.paidAmount;
 
         localStorage.setItem("editId", id);
+    })
+    .catch(err => {
+        console.error("EDIT ERROR:", err);
+        alert("Edit failed");
     });
-
 }
 
 function deleteStudent(mobile){
